47193f66e58808428ff3ece26eef492a3e3fa185
LaraMi92 <laraminski@hotmail.fr>
2020-12-31 12:09:00 +0100

    home hover effect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,42 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
 
 // == Import
 import './home.scss';
 
 // == Composant
-const Home = () => (
+const Home = () => {
+  const [isShown, setIsShown] = useState(true);
 
-  <div className="home">
-    <img
-      className="lieu-retour"
-      alt="lieu du retour"
-      src="https://db3pap004files.storage.live.com/y4msnzVm-3szm85ChF94zQUhjZ7_f4YmckwDvznV0-NheCKMHI70QE9_DghyHD1pAxXQdYHBc59rH5Wzu9lJvMycAzh0q8iaZB0cBKk25GMOci1GhfarecRhAS90JJr7FF4-AQrJpPFtV4HgQUZVfGEywCep3r1QIt4Rql9Kpaw8e-GcJwJmCcL6Kxz9m7REpHX?width=1748&height=1240&cropmode=none"
-    />
-    <div className="texte-retour"> Voilà.</div>
-  </div>
+  return (
+    <div
+      className="home"
+      onMouseOver={() => setIsShown(false)}
+      /* onMouseLeave={() => setIsShown(true)} */
+    >
 
-);
+      {isShown
+        ? (
+          <img
+            className="lieu-retour"
+            alt="lieu du retour"
+            src="https://db3pap004files.storage.live.com/y4msnzVm-3szm85ChF94zQUhjZ7_f4YmckwDvznV0-NheCKMHI70QE9_DghyHD1pAxXQdYHBc59rH5Wzu9lJvMycAzh0q8iaZB0cBKk25GMOci1GhfarecRhAS90JJr7FF4-AQrJpPFtV4HgQUZVfGEywCep3r1QIt4Rql9Kpaw8e-GcJwJmCcL6Kxz9m7REpHX?width=1748&height=1240&cropmode=none"
+          />
+        )
+        : (
+          <div className="texte">
+
+            <p>La citation appelle le mot par son nom, </p>
+            <p>l’arrache à son contexte en le détruisant, </p>
+            <p>mais par là même le rappelle aussi à son origine.</p>
+            <p>Le mot est sonore ainsi, cohérent, dans le cadre d’un texte nouveau ; </p>
+            <p>on ne peut pas dire qu’il ne rime à rien.</p>
+            <div className="auteur">Walter Benjamin</div>
+          </div>
+        )}
+    </div>
+  );
+};
 
 // == Export
 export default Home;
