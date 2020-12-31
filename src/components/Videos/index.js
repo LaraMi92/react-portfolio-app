dc5057590491f4283c7e7e1e4a09b2ba61b26e0a
LaraMi92 <laraminski@hotmail.fr>
2020-12-31 17:17:01 +0100

    media q

--- src/components/Videos/index.js
+++ src/components/Videos/index.js
@@ -8,33 +8,41 @@ import './videos.scss';
 const Videos = () => (
 
   <div className="videos">
-    <ReactPlayer
-      url="https://youtu.be/VDs9MecxDG0"
-      controls
-      width="400px"
-      height="300px"
-    />
+    <div className="player-wrapper">
+      <ReactPlayer
+        url="https://youtu.be/VDs9MecxDG0"
+        controls
+        width="100%"
+        height="100%"
+      />
+    </div>
     <div> <span className="bold">Le lieu du retour</span>, 2016. Vidéo exposée lors de Metasporas, au PointCulture de Bruxelles, 2020. </div>
-    <ReactPlayer
-      url="https://youtu.be/3wEWla-6aEc"
-      controls
-      width="400px"
-      height="300px"
-    />
+    <div className="player-wrapper">
+      <ReactPlayer
+        url="https://youtu.be/3wEWla-6aEc"
+        controls
+        width="100%"
+        height="100%"
+      />
+    </div>
     <div><span className="bold">Josée</span>, 2014. </div>
-    <ReactPlayer
-      url="https://youtu.be/hea1ehhdk7U"
-      controls
-      width="400px"
-      height="300px"
-    />
+    <div className="player-wrapper">
+      <ReactPlayer
+        url="https://youtu.be/hea1ehhdk7U"
+        controls
+        width="100%"
+        height="100%"
+      />
+    </div>
     <div><span className="bold">Champ du signe</span>, 2016. </div>
-    <ReactPlayer
-      url="https://youtu.be/oCJk3_uCv0E"
-      controls
-      width="400px"
-      height="300px"
-    />
+    <div className="player-wrapper">
+      <ReactPlayer
+        url="https://youtu.be/oCJk3_uCv0E"
+        controls
+        width="100%"
+        height="100%"
+      />
+    </div>
     <div><span className="bold">Kacper - Au même endroit</span>, 2015. Vidéo réalisée à l'occasion de l'exposition Atelier with a view (musée Art&marges, Bruxelles) en partenariat avec le centre Sésame.  </div>
   </div>
 );
